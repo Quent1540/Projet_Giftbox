ce8ba6f2a7e5d7084d0a79e20fbbb04c80433f7a
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-05-06 08:29:57 +0200

    Exercice 1
    Modif conf.ini
    Classe php Catégorie

---
+++ gift.appli/src/models/Categorie.php
@@ -0,0 +1,12 @@
+<?php
+namespace gift\appli\src\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model {
+    protected $table = 'categorie';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = ['id', 'libelle', 'description'];
+}
