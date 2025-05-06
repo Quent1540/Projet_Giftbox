bb0cde8d92e4ad3b27cde7febcde44da2e300dc8
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-05-06 09:37:19 +0200

    class Prestation

---
+++ giftAppli/src/models/Prestation.php
@@ -0,0 +1,14 @@
+<?php
+namespace gift\appli\models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Prestation extends Model {
+    protected $table = 'prestation';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    public $incrementing = false;
+    public $keyType = 'string';
+
+    protected $fillable = ['id', 'libelle', 'url', 'description', 'tarif', 'unité'];
+}
