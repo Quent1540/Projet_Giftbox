0f4199164d65480dd20452d4f60916d6a1c335ff
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-05-09 12:00:58 +0200

    TD2 qui marche

--- giftAppli/public/index.php
+++ giftAppli/public/index.php
@@ -1,8 +1,20 @@
 <?php
 declare(strict_types=1);
+//Chargement de l'autoloader
+require_once __DIR__. '/../src/vendor/autoload.php';
 
-session_start();
+use Slim\Factory\AppFactory;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-require_once __DIR__ . '/../src/vendor/autoload.php';
+//Création de l'application
+$app = AppFactory::create();
+//$app->setBasePath('/Projet_Giftbox/giftAppli/public');
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
 
 
+//Déclaration des routes
+$app = (require_once __DIR__ . '/../src/conf/routes.php')($app);
+
+$app->run();
