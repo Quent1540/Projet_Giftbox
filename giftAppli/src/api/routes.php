901e3463f5f8c52d5e3c888c1fe5d2077e6a0c56
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-06-03 15:38:12 +0200

    liens pour naviguer vers les APIs

--- giftAppli/src/api/routes.php
+++ giftAppli/src/api/routes.php
@@ -6,6 +6,6 @@ use Slim\App;
 return function (App $app): App{
     $app->get('/api/categories', \gift\appli\api\actions\GetCategoriesApiAction::class);
     $app->get('/api/prestations', \gift\appli\api\actions\GetPrestationsApiAction::class);
-    $app->get('/api/categories/{id}/prestations', \gift\appli\api\actions\GetPrestationsParCategorieApiAction::class);
+    $app->get('/api/categorie/{id}/prestations', \gift\appli\api\actions\GetPrestationsParCategorieApiAction::class);
     return $app;
 };
